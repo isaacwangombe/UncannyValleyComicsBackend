90ac51a8faeff6f2a3b32e20ef17dd88ad6696d9
isaac <isaac.wangombe@ukiyosoftware.com>
2025-11-04 07:34:29 +0300

    adding cors2

--- uncannyvalleycomics/settings.py
+++ uncannyvalleycomics/settings.py
@@ -327,7 +327,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
     "https://uncanny-valley-comics-backend.onrender.com",
-    "https://uncanny-valley-comics.onrender.com",
+    "https://uncannyvalleycomics.onrender.com",
     "https://uncannyvalleycomics.netlify.app",
 
 ]
@@ -335,7 +335,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
-    "https://uncanny-valley-comics.onrender.com",
+    "https://uncannyvalleycomics.onrender.com",
     "https://uncanny-valley-comics-backend.onrender.com",
     "https://uncannyvalleycomics.netlify.app",
 ]
