f31cdc181fc1af42019759fc7d90fa52b27dc91d
isaac <isaac.wangombe@ukiyosoftware.com>
2025-11-04 07:32:07 +0300

    adding cors

--- uncannyvalleycomics/settings.py
+++ uncannyvalleycomics/settings.py
@@ -327,6 +327,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
     "https://uncanny-valley-comics-backend.onrender.com",
+    "https://uncanny-valley-comics.onrender.com",
     "https://uncannyvalleycomics.netlify.app",
 
 ]
@@ -334,15 +335,11 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://uncanny-valley-comics.onrender.com",
     "https://uncanny-valley-comics-backend.onrender.com",
     "https://uncannyvalleycomics.netlify.app",
 ]
 
-# SESSION_COOKIE_SAMESITE = "None"
-# CSRF_COOKIE_SAMESITE = "None"
-
-# SESSION_COOKIE_SECURE =  os.getenv('SESSION_COOKIE_SECURE')
-# CSRF_COOKIE_SECURE =  os.getenv('CSRF_COOKIE_SECURE')
 
 CSRF_COOKIE_SAMESITE = "None"
 SESSION_COOKIE_SAMESITE = "None"
