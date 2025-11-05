4ef66c455da59fd8278e2a79ba943560747ef4b3
isaac <isaac.wangombe@ukiyosoftware.com>
2025-11-05 08:53:15 +0300

    Test2

--- uncannyvalleycomics/settings.py
+++ uncannyvalleycomics/settings.py
@@ -104,6 +104,8 @@ AUTH_USER_MODEL = "users.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
